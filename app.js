125e64604a45e96714de56d46a1a51c560254015
unknown <javimorenomontejo@gmail.com>
2021-10-12 19:12:40 +0200

    upgrades

--- app.js
+++ app.js
@@ -3,7 +3,8 @@ const getPokemons = async() => {
     const pokemonsJson = await pokemonData.json();
     const AllPokemons = pokemonsJson.results.map((nombre, imagen) => ({
         name: nombre.name,
-        img: `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/${imagen+1}.svg`,
+        img: `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${imagen+1}.png`,
+        shiny: `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/shiny/${imagen+1}.png`,
     }));
 
     paintPokemons(AllPokemons);
@@ -13,11 +14,30 @@ const list = document.querySelector(".pokemons");
 
 const paintPokemons = AllPokemons =>{
     const pokePaint = AllPokemons.map((element) => 
-        `<li>
+        `<li class="poke-contain">
         <h3>${element.name}</h3>
-        <img src="${element.img}" alt="${element.name}"</li>`
+        <img src="${element.img}" alt="${element.name}" id="${element.name}" onmouseout = "cambiarNormal('${element.name}', '${element.img}')" onMouseOver = "cambiarImagen('${element.name}', '${element.shiny}')"/>
+        `
     ).join('');
     list.innerHTML = pokePaint;
+    
 }
 
+function cambiarImagen(nombre, shiny){
+    let imagenPoke = document.getElementById(nombre);
+    let fondo = document.getElementById("fondo");
+    imagenPoke.src = shiny;
+}
+
+function cambiarNormal(nombre, imagen){
+    let imagenPoke = document.getElementById(nombre);
+    imagenPoke.src = imagen;
+}
+
+
+
+
+
+
 getPokemons();
+
